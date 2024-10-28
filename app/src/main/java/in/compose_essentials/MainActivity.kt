11831f5b89908282942a785b2a69f335e2be12d5
nagesh <nageswar.manda@vaibhav-vyapaar.com>
2024-10-28 23:51:50 +0530

    textview

--- app/src/main/java/in/compose_essentials/MainActivity.kt
+++ app/src/main/java/in/compose_essentials/MainActivity.kt
@@ -10,7 +10,17 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import `in`.compose_essentials.ui.theme.ComposeessentialsTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +44,30 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        color = Color.Red,
+        fontSize =dimensionResource(id = R.dimen._18sp).value.sp,
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.Black,
+        fontFamily = FontFamily.Monospace,
+        letterSpacing =dimensionResource(id = R.dimen._2sp).value.sp,
+        textDecoration = TextDecoration.Underline,
+        textAlign = TextAlign.Center,
+        lineHeight =dimensionResource(id = R.dimen._2sp).value.sp,
+        maxLines = 1,
+        minLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        softWrap = false,
+        onTextLayout={ textLayoutResult ->
+            if (textLayoutResult.lineCount > 0){
+                println("Text exceeds 2 lines")
+            }
+        },
+        style = TextStyle(
+            color = Color.Blue,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold
+        )
     )
 }
 
